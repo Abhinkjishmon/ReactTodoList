71c1c9d868c33d0172bdcd1be02cdcea93464917
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-01-18 20:03:09 +0530

    second commit

--- src/component/TodoApp/TodoApp.js
+++ src/component/TodoApp/TodoApp.js
@@ -2,23 +2,54 @@ import React, { Component } from 'react';
 import "./TodoApp.css";
 
 export default class TodoApp extends Component {
+    state = {
+        input: "",
+        items: []
+    }
+    handleChange = (event)=>{
+        this.setState({
+            input: event.target.value
+        })
+    };
+    storeItems =(event)=>{
+        event.preventDefault();
+        const {input} = this.state;
+
+        this.setState({
+            items: [...this.state.items, input],
+            input: ""
+
+        })
+    }
+    deleteItem =(index)=>{
+        const allItems = this.state.items;
+
+        allItems.splice(index,1);
+
+        this.setState({
+            items: allItems
+        })
+    }
     render() {
+        const { input, items } = this.state;
+        console.log(items);
         return (
             <div className='todo-container'>
 
-
-                <form className='input-section'>
+                <form className='input-section' onSubmit={this.storeItems}>
                     <h1>Todo App</h1>
-                    <input type="text" placeholder='Enter Items.....' />
+                    <input type="text" value={input} onChange={this.handleChange} placeholder='Enter Items.....' />
 
                 </form>
-
                 <ul>
-                    <li>items <i className="fa-solid fa-trash"></i></li>
-                    <li>items <i className="fa-solid fa-trash"></i></li>
-                    <li>items <i className="fa-solid fa-trash"></i></li>
+                    {items.map((data,index)=>(
+                        <li key={index}>
+                            {data}<i className="fa-solid fa-trash" onClick={()=>this.deleteItem(index)}></i>
+                        </li>
+                    ))}
                 </ul>
             </div>
         )
     }
 }
+
